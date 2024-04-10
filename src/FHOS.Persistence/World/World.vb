90ff9b2c51f3c9449e3d64e7879f82515b1eaca8
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-04-10 05:03:42 -0500

    next pivot towards recycled things

--- src/FHOS.Persistence/World/World.vb
+++ src/FHOS.Persistence/World/World.vb
@@ -22,25 +22,27 @@ Public Class World
 
     Public Function CreateMap(columns As Integer, rows As Integer, terrainType As String) As IMap Implements IWorld.CreateMap
         Dim mapId = WorldData.Maps.Count
-        WorldData.Maps.Add(New MapData With
+        Dim mapData = New MapData With
             {
                 .Columns = columns,
                 .Rows = rows,
                 .Cells = Enumerable.
                     Range(0, columns * rows).
                     Select(Function(x) CreateCell(terrainType, mapId, x Mod rows, x \ rows).Id).ToList
-            })
+            }
+        WorldData.Maps.Add(mapData)
         Return New Map(WorldData, mapId)
     End Function
 
     Public Function CreateCharacter(characterType As String, cell As ICell) As ICharacter Implements IWorld.CreateCharacter
         Dim characterId = WorldData.Characters.Count
-        WorldData.Characters.Add(New CharacterData With
+        Dim characterData = New CharacterData With
                                  {
                                     .CharacterType = characterType,
                                     .Facing = 0,
                                     .CellId = cell.Id
-                                 })
+                                 }
+        WorldData.Characters.Add(characterData)
         Dim character = New Character(WorldData, characterId)
         cell.Character = character
         Return character
@@ -48,14 +50,15 @@ Public Class World
 
     Public Function CreateCell(terrainType As String, mapId As Integer, column As Integer, row As Integer) As ICell Implements IWorld.CreateCell
         Dim cellId = WorldData.Cells.Count
-        WorldData.Cells.Add(New CellData With
+        Dim cellData = New CellData With
                             {
                                 .TerrainType = terrainType,
                                 .MapId = mapId,
                                 .Column = column,
                                 .Row = row,
                                 .CharacterId = Nothing
-                            })
+                            }
+        WorldData.Cells.Add(cellData)
         Return New Cell(WorldData, cellId)
     End Function
 End Class
