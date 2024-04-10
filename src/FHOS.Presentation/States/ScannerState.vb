5861d8734493f91003b1efb7ce4123b865de7eb5
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-04-10 05:53:43 -0500

    a rough scanner state

--- src/FHOS.Presentation/States/ScannerState.vb
+++ src/FHOS.Presentation/States/ScannerState.vb
@@ -3,6 +3,7 @@ Imports SPLORR.UI
 
 Friend Class ScannerState
     Inherits BoardState
+    Private target As (X As Integer, Y As Integer) = (ViewColumns \ 2, ViewRows \ 2)
 
     Public Sub New(parent As IGameController, setState As Action(Of String, Boolean), context As IUIContext(Of IWorldModel))
         MyBase.New(parent, setState, context)
@@ -12,7 +13,19 @@ Friend Class ScannerState
         Select Case cmd
             Case Command.Select
                 SetState(GameState.Navigation)
+            Case Command.Up
+                MoveTarget(0, -1)
+            Case Command.Down
+                MoveTarget(0, 1)
+            Case Command.Left
+                MoveTarget(-1, 0)
+            Case Command.Right
+                MoveTarget(1, 0)
         End Select
+    End Sub
+
+    Private Sub MoveTarget(deltaX As Integer, deltaY As Integer)
+        target = (target.X + deltaX, target.Y + deltaY)
     End Sub
 
     Public Overrides Sub Render(displayBuffer As IPixelSink)
@@ -25,5 +38,6 @@ Friend Class ScannerState
             uiFont,
             (0, 0),
             (cellWidth, cellHeight))
+        uiFont.WriteText(displayBuffer, (cellWidth * target.X, cellHeight * target.Y), ChrW(16), 4)
     End Sub
 End Class
