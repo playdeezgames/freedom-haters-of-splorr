e832e072737d481d92999c72d02b115b32611dff
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-04-10 05:01:22 -0500

    infrastructure for recycled entities

--- src/FHOS.Data/WorldData.vb
+++ src/FHOS.Data/WorldData.vb
@@ -1,6 +1,9 @@
 Public Class WorldData
     Property AvatarId As Integer?
     Property Maps As New List(Of MapData)
+    Property RecycledMaps As New HashSet(Of Integer)
     Property Characters As New List(Of CharacterData)
+    Property RecycledCharacters As New HashSet(Of Integer)
     Property Cells As New List(Of CellData)
+    Property RecycledCells As New HashSet(Of Integer)
 End Class
