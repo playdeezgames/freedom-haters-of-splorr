65b5bdabc692740a2b5029ef5633ce0adceff92e
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-04-19 04:35:13 -0500

    base message state

--- src/SPLORR.UI/Boilerplate/Informational/SaveConfirmationState.vb
+++ src/SPLORR.UI/Boilerplate/Informational/SaveConfirmationState.vb
@@ -1,30 +1,22 @@
 ﻿Friend Class SaveConfirmationState(Of TModel)
-    Inherits BaseGameState(Of TModel)
+    Inherits BaseMessageState(Of TModel)
 
     Public Sub New(
                   parent As IGameController,
                   setState As Action(Of String, Boolean),
                   context As IUIContext(Of TModel))
-        MyBase.New(parent, setState, context)
+        MyBase.New(
+            parent,
+            setState,
+            context,
+            BoilerplateState.GameMenu)
     End Sub
 
-    Public Overrides Sub HandleCommand(cmd As String)
-        SetState(BoilerplateState.GameMenu)
-    End Sub
+    Protected Overrides Function MessageHue() As Integer
+        Return Context.UIPalette.MenuItem
+    End Function
 
-    Public Overrides Sub Render(displayBuffer As IPixelSink)
-        displayBuffer.Fill(Context.UIPalette.Background)
-        Dim font = Context.Font(UIFont)
-        font.WriteCenteredText(
-            displayBuffer,
-            (Context.ViewCenter.X, Context.ViewCenter.Y - font.HalfHeight),
-            "Game Saved.",
-            Context.UIPalette.MenuItem)
-        Context.ShowStatusBar(
-            displayBuffer,
-            font,
-            Context.ControlsText(aButton:="Continue"),
-            Context.UIPalette.Background,
-            Context.UIPalette.Footer)
-    End Sub
+    Protected Overrides Function MessageText() As String
+        Return "Game Saved."
+    End Function
 End Class
