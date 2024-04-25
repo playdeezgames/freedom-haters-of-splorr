3817cf599d7cacc71a27588e9bd773b64f6a6d71
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-04-25 15:41:31 -0500

    ss and iactor org

--- src/FHOS.Persistence/Actor/IActor.vb
+++ src/FHOS.Persistence/Actor/IActor.vb
@@ -1,24 +1,31 @@
 ﻿Public Interface IActor
     ReadOnly Property Id As Integer
-    Property Location As ILocation
     ReadOnly Property ActorType As String
+    Property Location As ILocation
+    Property Facing As Integer
+
     Property MaximumOxygen As Integer
     Property Oxygen As Integer
+
     Property MaximumFuel As Integer
     Property Fuel As Integer
-    Property Facing As Integer
+    ReadOnly Property HasFuel As Boolean
+
     ReadOnly Property HasTutorial As Boolean
     ReadOnly Property CurrentTutorial As String
+    Sub DismissTutorial()
+    Sub TriggerTutorial(tutorial As String)
+
     ReadOnly Property KnowsStarSystems As Boolean
     ReadOnly Property KnownStarSystems As IEnumerable(Of IStarSystem)
-    Sub DismissTutorial()
-    Sub SetFlag(flag As String)
     Sub AddStarSystem(starSystem As IStarSystem)
+
+    Sub SetFlag(flag As String)
     Function HasFlag(flag As String) As Boolean
+
     Property Turn As Integer
     Property Jools As Integer
-    Sub TriggerTutorial(tutorial As String)
-    ReadOnly Property HasFuel As Boolean
+
     Sub AddMessage(
                   header As String,
                   ParamArray lines As (Text As String, Hue As Integer)())
