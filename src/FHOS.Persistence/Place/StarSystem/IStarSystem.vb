697fd18765dc5d10c4c672c2df1f2f12963628e6
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-05-01 04:12:57 -0500

    star system interface inherits from place interface

--- src/FHOS.Persistence/Place/StarSystem/IStarSystem.vb
+++ src/FHOS.Persistence/Place/StarSystem/IStarSystem.vb
@@ -1,9 +1,8 @@
 ﻿Public Interface IStarSystem
-    ReadOnly Property Id As Integer
+    Inherits IPlace
     ReadOnly Property Name As String
     ReadOnly Property Identifier As String
     Property Map As IMap
-    ReadOnly Property Universe As IUniverse
     ReadOnly Property StarType As String
     Function CreateStarVicinity() As IStarVicinity
     Function CreatePlanetVicinity(planetName As String, planetType As String) As IPlanetVicinity
