a9dbfb32045b7d93c4cfe40585b99d7529040612
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-05-07 04:21:03 -0500

    5x5 planet

--- src/FHOS.Model/Initializer/Steps/PlanetInitializationStep.vb
+++ src/FHOS.Model/Initializer/Steps/PlanetInitializationStep.vb
@@ -17,6 +17,7 @@ Friend Class PlanetInitializationStep
             PlanetOrbitRows,
             LocationTypes.Void)
         PlaceBoundaries(planet, location)
+        PlacePlanet(planet)
         planet.Map.Place = planet
     End Sub
     Private Sub PlaceBoundaries(planet As IPlace, location As ILocation)
@@ -29,5 +30,48 @@ Friend Class PlanetInitializationStep
             PlaceBoundary(planet.Map.GetLocation(edge.X, edge.Y), edge.LocationType, teleporter)
             planet.Map.GetLocation(edge.X + edge.DeltaX, edge.Y + edge.DeltaY).SetFlag(identifier)
         Next
+    End Sub
+    Private ReadOnly planetSectionDeltas As IReadOnlyList(Of (DeltaX As Integer, DeltaY As Integer, SectionName As String)) =
+        New List(Of (DeltaX As Integer, DeltaY As Integer, SectionName As String)) From
+        {
+            (-2, -2, LocationTypes.C1R1),
+            (-1, -2, LocationTypes.C2R1),
+            (0, -2, LocationTypes.C3R1),
+            (1, -2, LocationTypes.C4R1),
+            (2, -2, LocationTypes.C5R1),
+            (-2, -1, LocationTypes.C1R2),
+            (-1, -1, LocationTypes.C2R2),
+            (0, -1, LocationTypes.C3R2),
+            (1, -1, LocationTypes.C4R2),
+            (2, -1, LocationTypes.C5R2),
+            (-2, 0, LocationTypes.C1R3),
+            (-1, 0, LocationTypes.C2R3),
+            (0, 0, LocationTypes.C3R3),
+            (1, 0, LocationTypes.C4R3),
+            (2, 0, LocationTypes.C5R3),
+            (-2, 1, LocationTypes.C1R4),
+            (-1, 1, LocationTypes.C2R4),
+            (0, 1, LocationTypes.C3R4),
+            (1, 1, LocationTypes.C4R4),
+            (2, 1, LocationTypes.C5R4),
+            (-2, 2, LocationTypes.C1R5),
+            (-1, 2, LocationTypes.C2R5),
+            (0, 2, LocationTypes.C3R5),
+            (1, 2, LocationTypes.C4R5),
+            (2, 2, LocationTypes.C5R5)
+        }
+    Private Sub PlacePlanet(planet As IPlace)
+        Dim planetCenterColumn = PlanetOrbitColumns \ 2
+        Dim planetCenterRow = PlanetOrbitRows \ 2
+        For Each delta In planetSectionDeltas
+            PlacePlanetSection(planet.PlanetType, planet.Map.GetLocation(planetCenterColumn + delta.DeltaX, planetCenterRow + delta.DeltaY), delta.SectionName)
+        Next
+    End Sub
+    Private Shared Sub PlacePlanetSection(planetType As String, location As ILocation, sectionName As String)
+        Dim locationType = PlanetTypes.Descriptors(planetType).SectionLocationType(sectionName)
+        With location
+            .LocationType = locationType
+            .Tutorial = TutorialTypes.EnterPlanetOrbit
+        End With
     End Sub
 End Class
--- src/FHOS.Model/Location/LocationType/LocationTypes.vb
+++ src/FHOS.Model/Location/LocationType/LocationTypes.vb
@@ -22,6 +22,7 @@
         Return $"{MakePlanetLocationType(planetType)}{sectionName}"
     End Function
 
+
     Friend Const RadiatedMoon = "RadiatedMoon"
     Friend Const VolcanicMoon = "VolcanicMoon"
     Friend Const BarrenMoon = "BarrenMoon"
@@ -59,7 +60,7 @@
     Friend Const BottomLeft = "BottomLeft"
     Friend Const BottomCenter = "BottomCenter"
     Friend Const BottomRight = "BottomRight"
-    Private ReadOnly planetSectionTable As IReadOnlyDictionary(Of String, Char) =
+    Private ReadOnly planet3x3SectionTable As IReadOnlyDictionary(Of String, Char) =
         New Dictionary(Of String, Char) From
         {
             {TopLeft, ChrW(229)},
@@ -71,6 +72,60 @@
             {BottomLeft, ChrW(233)},
             {BottomCenter, ChrW(234)},
             {BottomRight, ChrW(235)}
+        }
+    Friend Const C1R1 = "C1R1"
+    Friend Const C1R2 = "C1R2"
+    Friend Const C1R3 = "C1R3"
+    Friend Const C1R4 = "C1R4"
+    Friend Const C1R5 = "C1R5"
+    Friend Const C2R1 = "C2R1"
+    Friend Const C2R2 = "C2R2"
+    Friend Const C2R3 = "C2R3"
+    Friend Const C2R4 = "C2R4"
+    Friend Const C2R5 = "C2R5"
+    Friend Const C3R1 = "C3R1"
+    Friend Const C3R2 = "C3R2"
+    Friend Const C3R3 = "C3R3"
+    Friend Const C3R4 = "C3R4"
+    Friend Const C3R5 = "C3R5"
+    Friend Const C4R1 = "C4R1"
+    Friend Const C4R2 = "C4R2"
+    Friend Const C4R3 = "C4R3"
+    Friend Const C4R4 = "C4R4"
+    Friend Const C4R5 = "C4R5"
+    Friend Const C5R1 = "C5R1"
+    Friend Const C5R2 = "C5R2"
+    Friend Const C5R3 = "C5R3"
+    Friend Const C5R4 = "C5R4"
+    Friend Const C5R5 = "C5R5"
+    Private ReadOnly planet5x5SectionTable As IReadOnlyDictionary(Of String, Char) =
+        New Dictionary(Of String, Char) From
+        {
+            {C1R1, ChrW(236)},
+            {C2R1, ChrW(237)},
+            {C3R1, ChrW(230)},
+            {C4R1, ChrW(238)},
+            {C5R1, ChrW(239)},
+            {C1R2, ChrW(240)},
+            {C2R2, ChrW(230)},
+            {C3R2, ChrW(230)},
+            {C4R2, ChrW(230)},
+            {C5R2, ChrW(241)},
+            {C1R3, ChrW(230)},
+            {C2R3, ChrW(230)},
+            {C3R3, ChrW(230)},
+            {C4R3, ChrW(230)},
+            {C5R3, ChrW(232)},
+            {C1R4, ChrW(242)},
+            {C2R4, ChrW(230)},
+            {C3R4, ChrW(230)},
+            {C4R4, ChrW(230)},
+            {C5R4, ChrW(243)},
+            {C1R5, ChrW(244)},
+            {C2R5, ChrW(245)},
+            {C3R5, ChrW(234)},
+            {C4R5, ChrW(246)},
+            {C5R5, ChrW(247)}
         }
     Private Function GenerateLocationTypes() As IReadOnlyDictionary(Of String, LocationTypeDescriptor)
         Dim result = New Dictionary(Of String, LocationTypeDescriptor) From
@@ -99,7 +154,10 @@
         }
         For Each planetHue In planetHueTable
             result(MakePlanetLocationType(planetHue.Key)) = New LocationTypeDescriptor($"{planetHue.Key} Planet", ChrW(225), planetHue.Value, Black)
-            For Each planetSection In planetSectionTable
+            For Each planetSection In planet3x3SectionTable
+                result(MakePlanetSectionLocationType(planetHue.Key, planetSection.Key)) = New LocationTypeDescriptor($"{planetHue.Key} Planet", planetSection.Value, planetHue.Value, Black)
+            Next
+            For Each planetSection In planet5x5SectionTable
                 result(MakePlanetSectionLocationType(planetHue.Key, planetSection.Key)) = New LocationTypeDescriptor($"{planetHue.Key} Planet", planetSection.Value, planetHue.Value, Black)
             Next
         Next
