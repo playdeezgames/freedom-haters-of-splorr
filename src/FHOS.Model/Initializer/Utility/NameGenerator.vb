672ed7643daa1d21c54c0703492ff0ff1734ddd9
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-05-13 15:41:10 -0500

    changed name generator

--- src/FHOS.Model/Initializer/Utility/NameGenerator.vb
+++ src/FHOS.Model/Initializer/Utility/NameGenerator.vb
@@ -15,16 +15,43 @@ Friend Class NameGenerator
         Return name
     End Function
 
-    Const vowels = "aeiou"
-    Const consonants = "bdfghklmnprstv"
+    Private ReadOnly vowelTable As IReadOnlyDictionary(Of String, Integer) = New Dictionary(Of String, Integer) From
+        {
+            {"a", 1},
+            {"e", 1},
+            {"i", 1},
+            {"o", 1},
+            {"u", 1}
+        }
+    Private ReadOnly consonantTable As IReadOnlyDictionary(Of String, Integer) = New Dictionary(Of String, Integer) From
+        {
+            {"b", 1},
+            {"ch", 1},
+            {"d", 1},
+            {"f", 1},
+            {"g", 1},
+            {"k", 1},
+            {"l", 1},
+            {"m", 1},
+            {"n", 1},
+            {"p", 1},
+            {"r", 1},
+            {"s", 1},
+            {"sh", 1},
+            {"t", 1},
+            {"th", 1},
+            {"v", 1},
+            {"z", 1},
+            {"zh", 1}
+        }
     Private Function GenerateName() As String
         Dim builder As New StringBuilder
         Dim isVowel = RNG.FromEnumerable({False, True})
-        For Each index In Enumerable.Range(0, RNG.RollDice("4d3"))
+        For Each index In Enumerable.Range(0, RNG.RollDice("3d4"))
             If isVowel Then
-                builder.Append(RNG.FromEnumerable(vowels))
+                builder.Append(RNG.FromGenerator(vowelTable))
             Else
-                builder.Append(RNG.FromEnumerable(consonants))
+                builder.Append(RNG.FromGenerator(consonantTable))
             End If
             isVowel = Not isVowel
         Next
