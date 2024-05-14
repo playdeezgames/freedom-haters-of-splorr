4e8ecf8e8541fd0ddc6a942f8e0b04b0bb4f3389
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-05-14 13:32:45 -0500

    costume descriptors

---
+++ src/FHOS.Model/Costume/CostumeTypes.vb
@@ -0,0 +1,11 @@
+﻿Friend Module CostumeTypes
+    Friend ReadOnly MerchantVessel As String = NameOf(MerchantVessel)
+    Friend ReadOnly MilitaryVessel As String = NameOf(MilitaryVessel)
+
+    Friend ReadOnly Descriptors As IReadOnlyDictionary(Of String, CostumeTypeDescriptor) =
+        New List(Of CostumeTypeDescriptor) From
+        {
+            New CostumeTypeDescriptor(MerchantVessel),
+            New CostumeTypeDescriptor(MilitaryVessel)
+        }.ToDictionary(Function(x) x.Name, Function(x) x)
+End Module
