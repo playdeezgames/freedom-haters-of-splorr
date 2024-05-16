a9be93053652d8d7ce784556dbbedeeb0174599f
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-05-16 05:06:43 -0500

    there are 34 loights!

--- src/FHOS.Persistence/Actor/IActor.vb
+++ src/FHOS.Persistence/Actor/IActor.vb
@@ -5,25 +5,20 @@
     Property Facing As Integer
     Property Name As String
     Property Costume As String
-
     Property LifeSupport As IStore
-
     Property MaximumFuel As Integer
     Property Fuel As Integer
     ReadOnly Property HasFuel As Boolean
     Property ConsumesFuel As Boolean
-
     ReadOnly Property HasTutorial As Boolean
     ReadOnly Property CurrentTutorial As String
     Sub DismissTutorial()
     Sub TriggerTutorial(tutorial As String)
-
     ReadOnly Property KnowsPlaces As Boolean
     ReadOnly Property KnownPlaces As IEnumerable(Of IPlace)
     Function KnowsPlacesOfType(placeType As String) As Boolean
     Function GetKnownPlacesOfType(placeType As String) As IEnumerable(Of IPlace)
     Sub AddKnownPlace(place As IPlace)
-
     Property Jools As Integer
     Property MinimumJools As Integer
     Property Wallet As IStore
