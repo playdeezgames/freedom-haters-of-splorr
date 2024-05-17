dd65abad0cc77197e465847641cd91c9cbf83eaf
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-05-17 05:40:18 -0500

    rearrange pre refactor

--- src/FHOS.Persistence/Actor/IActor.vb
+++ src/FHOS.Persistence/Actor/IActor.vb
@@ -1,33 +1,47 @@
 ﻿Public Interface IActor
     Inherits IEntity
-    ReadOnly Property ActorType As String
-    Property Location As ILocation
-    Property Facing As Integer
-    Property Name As String
-    Property Costume As String
-    Property LifeSupport As IStore
+    'Fuel
     Property MaximumFuel As Integer
     Property Fuel As Integer
     ReadOnly Property HasFuel As Boolean
     Property ConsumesFuel As Boolean
+
+    'Tutorial
     ReadOnly Property HasTutorial As Boolean
     ReadOnly Property CurrentTutorial As String
     Sub DismissTutorial()
     Sub TriggerTutorial(tutorial As String)
+
+    'Known Places
     ReadOnly Property KnowsPlaces As Boolean
     ReadOnly Property KnownPlaces As IEnumerable(Of IPlace)
     Function KnowsPlacesOfType(placeType As String) As Boolean
     Function GetKnownPlacesOfType(placeType As String) As IEnumerable(Of IPlace)
     Sub AddKnownPlace(place As IPlace)
-    Property Jools As Integer
-    Property MinimumJools As Integer
-    Property Wallet As IStore
-    Property Faction As IFaction
-    Property HomePlanet As IPlace
-    Property Interactor As IActor
+
+    'Vessel
     Property Interior As IMap
     Sub AddCrew(crew As IActor)
     Property Vessel As IActor
     ReadOnly Property HasCrew As Boolean
     ReadOnly Property Crew As IEnumerable(Of IActor)
+    Property LifeSupport As IStore
+
+    'Money
+    Property Jools As Integer
+    Property MinimumJools As Integer
+    Property Wallet As IStore
+
+    'Status
+    ReadOnly Property ActorType As String
+    Property Name As String
+    Property Faction As IFaction
+    Property HomePlanet As IPlace
+
+    'State
+    Property Location As ILocation
+    Property Facing As Integer
+    Property Costume As String
+    Property Interactor As IActor
+
 End Interface
