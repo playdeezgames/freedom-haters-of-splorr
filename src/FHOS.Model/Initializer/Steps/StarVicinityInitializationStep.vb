2e85b003ab7b739ba69e4acfdf77899bb0aecf0f
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-05-17 04:28:56 -0500

    star vicinity

--- src/FHOS.Model/Initializer/Steps/StarVicinityInitializationStep.vb
+++ src/FHOS.Model/Initializer/Steps/StarVicinityInitializationStep.vb
@@ -2,26 +2,25 @@
 
 Friend Class StarVicinityInitializationStep
     Inherits InitializationStep
-    Private Const StarVicinityColumns = 15
-    Private Const StarVicinityRows = 15
     Private ReadOnly starLocation As ILocation
     Sub New(location As ILocation)
         Me.starLocation = location
     End Sub
     Public Overrides Sub DoStep(addStep As Action(Of InitializationStep, Boolean))
+        Dim descriptor = MapTypes.Descriptors(MapTypes.StarVicinity)
         Dim starVicinity = starLocation.Place
         starVicinity.Map = starVicinity.Universe.CreateMap(
             $"{starVicinity.Name} Vicinity",
-            MapTypes.StarVicinity,
-            StarVicinityColumns,
-            StarVicinityRows,
+            descriptor.MapType,
+            descriptor.Size.Columns,
+            descriptor.Size.Rows,
             LocationTypes.Void)
-        PlaceBoundaries(starVicinity, starLocation, StarVicinityColumns, StarVicinityRows)
+        PlaceBoundaries(starVicinity, starLocation, descriptor.Size.Columns, descriptor.Size.Rows)
         PlaceStar(starVicinity)
     End Sub
     Private Sub PlaceStar(starVicinity As IPlace)
-        Dim starColumn = StarVicinityColumns \ 2
-        Dim starRow = StarVicinityRows \ 2
+        Dim starColumn = starVicinity.Map.Size.Columns \ 2
+        Dim starRow = starVicinity.Map.Size.Rows \ 2
         Dim locationType = StarTypes.Descriptors(starVicinity.StarType).LocationType
         Dim location = starVicinity.Map.GetLocation(starColumn, starRow)
         With location
