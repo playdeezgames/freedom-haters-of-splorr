1a66cf6fbf6d3639beba0214dea8b4d240e7d8d3
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-05-19 21:03:24 -0500

    added verbtype to verb descriptor parameters

--- src/FHOS.Model/VerbType/VerbTypeDescriptor.vb
+++ src/FHOS.Model/VerbType/VerbTypeDescriptor.vb
@@ -1,15 +1,18 @@
 ﻿Imports FHOS.Persistence
 
 Friend Class VerbTypeDescriptor
+    Friend ReadOnly Property VerbType As String
     Friend ReadOnly Property Text As String
     Friend ReadOnly Property Visible As Boolean
     Private ReadOnly checkAvailability As Func(Of IActor, Boolean)
     Private ReadOnly doPerform As Action(Of IActor)
     Friend Sub New(
+                  verbType As String,
                   text As String,
                   checkAvailability As Func(Of IActor, Boolean),
                   doPerform As Action(Of IActor),
                   Optional visible As Boolean = True)
+        Me.VerbType = verbType
         Me.Text = text
         Me.Visible = visible
         Me.checkAvailability = checkAvailability
--- src/FHOS.Model/VerbType/VerbTypes.vb
+++ src/FHOS.Model/VerbType/VerbTypes.vb
@@ -23,7 +23,7 @@ Public Module VerbTypes
     Friend ReadOnly Descriptors As IReadOnlyDictionary(Of String, VerbTypeDescriptor) =
         New Dictionary(Of String, VerbTypeDescriptor) From
         {
-            {EnterStarSystem, New VerbTypeDescriptor(
+            {EnterStarSystem, New VerbTypeDescriptor(EnterStarSystem,
                 "Enter Star System",
                 Function(actor) actor.State.Location.Place?.PlaceType = PlaceTypes.StarSystem,
                 Sub(actor)
@@ -32,14 +32,14 @@ Public Module VerbTypes
                         SetLocation(actor, RNG.FromEnumerable(.Properties.Map.Locations.Where(Function(x) x.Flags(.Properties.Identifier) AndAlso x.Actor Is Nothing)))
                     End With
                 End Sub)},
-            {ApproachPlanetVicinity, New VerbTypeDescriptor("Approach Planet", Function(actor) actor.State.Location.Place?.PlaceType = PlaceTypes.PlanetVicinity,
+            {ApproachPlanetVicinity, New VerbTypeDescriptor(ApproachPlanetVicinity, "Approach Planet", Function(actor) actor.State.Location.Place?.PlaceType = PlaceTypes.PlanetVicinity,
                 Sub(actor)
                     DoTurn(actor)
                     With actor.State.Location.Place
                         SetLocation(actor, RNG.FromEnumerable(.Properties.Map.Locations.Where(Function(x) x.Flags(.Properties.Identifier) AndAlso x.Actor Is Nothing)))
                     End With
                 End Sub)},
-            {ApproachStarVicinity, New VerbTypeDescriptor("Approach Star", Function(actor) actor.State.Location.Place?.PlaceType = PlaceTypes.StarVicinity,
+            {ApproachStarVicinity, New VerbTypeDescriptor(ApproachStarVicinity, "Approach Star", Function(actor) actor.State.Location.Place?.PlaceType = PlaceTypes.StarVicinity,
                 Sub(actor)
                     DoTurn(actor)
                     With actor.State.Location.Place
@@ -47,45 +47,45 @@ Public Module VerbTypes
                     End With
 
                 End Sub)},
-            {RefillOxygen, New VerbTypeDescriptor("Refill Oxygen", Function(actor)
-                                                                       Dim placeType = actor.State.Location.Place?.PlaceType
-                                                                       If placeType <> PlaceTypes.Planet Then
-                                                                           Return False
-                                                                       End If
-                                                                       Return PlanetTypes.Descriptors(actor.State.Location.Place.Subtype).CanRefillOxygen
-                                                                   End Function,
+            {RefillOxygen, New VerbTypeDescriptor(RefillOxygen, "Refill Oxygen", Function(actor)
+                                                                                     Dim placeType = actor.State.Location.Place?.PlaceType
+                                                                                     If placeType <> PlaceTypes.Planet Then
+                                                                                         Return False
+                                                                                     End If
+                                                                                     Return PlanetTypes.Descriptors(actor.State.Location.Place.Subtype).CanRefillOxygen
+                                                                                 End Function,
                 Sub(actor)
                     actor.State.LifeSupport.CurrentValue = actor.State.LifeSupport.MaximumValue.Value
 
                 End Sub)},
-            {Refuel, New VerbTypeDescriptor("Refuel", Function(actor)
-                                                          Return actor.State.Location.Place?.PlaceType = PlaceTypes.Star
-                                                      End Function,
+            {Refuel, New VerbTypeDescriptor(Refuel, "Refuel", Function(actor)
+                                                                  Return actor.State.Location.Place?.PlaceType = PlaceTypes.Star
+                                                              End Function,
                 Sub(actor)
                     actor.State.FuelTank.CurrentValue = actor.State.FuelTank.MaximumValue.Value
 
                 End Sub)},
-            {EnterWormhole, New VerbTypeDescriptor("Enter Wormhole", Function(actor)
-                                                                         Return actor.State.Location.Place?.PlaceType = PlaceTypes.Wormhole
-                                                                     End Function,
+            {EnterWormhole, New VerbTypeDescriptor(EnterWormhole, "Enter Wormhole", Function(actor)
+                                                                                        Return actor.State.Location.Place?.PlaceType = PlaceTypes.Wormhole
+                                                                                    End Function,
                 Sub(actor)
                     DoTurn(actor)
                     With actor.State.Location.Place
                         SetLocation(actor, .Properties.WormholeDestination)
                     End With
                 End Sub)},
-            {EnterOrbit, New VerbTypeDescriptor("Enter Orbit", Function(actor)
-                                                                   Return actor.State.Location.Place?.PlaceType = PlaceTypes.Planet OrElse actor.State.Location.Place?.PlaceType = PlaceTypes.Satellite
-                                                               End Function,
+            {EnterOrbit, New VerbTypeDescriptor(EnterOrbit, "Enter Orbit", Function(actor)
+                                                                               Return actor.State.Location.Place?.PlaceType = PlaceTypes.Planet OrElse actor.State.Location.Place?.PlaceType = PlaceTypes.Satellite
+                                                                           End Function,
                 Sub(actor)
                     DoTurn(actor)
                     With actor.State.Location.Place
                         SetLocation(actor, RNG.FromEnumerable(.Properties.Map.Locations.Where(Function(x) x.Flags(.Properties.Identifier) AndAlso x.Actor Is Nothing)))
                     End With
                 End Sub)},
-            {KnownPlaces, New VerbTypeDescriptor("Known Places...", Function(actor)
-                                                                        Return actor.KnownPlaces.HasAny
-                                                                    End Function,
+            {KnownPlaces, New VerbTypeDescriptor(KnownPlaces, "Known Places...", Function(actor)
+                                                                                     Return actor.KnownPlaces.HasAny
+                                                                                 End Function,
                 Sub(actor)
                     DoTurn(actor)
                     With actor.State.Location.Place
@@ -93,10 +93,10 @@ Public Module VerbTypes
                     End With
 
                 End Sub)},
-            {DistressSignal, New VerbTypeDescriptor("Signal Distress", Function(actor)
-                                                                           Return actor.State.FuelTank IsNot Nothing AndAlso
+            {DistressSignal, New VerbTypeDescriptor(DistressSignal, "Signal Distress", Function(actor)
+                                                                                           Return actor.State.FuelTank IsNot Nothing AndAlso
     AvatarModel.FromActor(actor).Vessel.FuelPercent.Value = 0
-                                                                       End Function,
+                                                                                       End Function,
                 Sub(actor)
                     Dim fuelAdded = actor.State.FuelTank.MaximumValue.Value - actor.State.FuelTank.CurrentValue
                     Dim fuelPrice = 1 'TODO: don't just pick a magic number!
@@ -109,52 +109,52 @@ Public Module VerbTypes
             ($"Price {price} jools!", Hues.Black))
 
                 End Sub)},
-            {Status, New VerbTypeDescriptor("Status", Function(actor) True,
+            {Status, New VerbTypeDescriptor(Status, "Status", Function(actor) True,
                 Sub(actor)
                     Return
                 End Sub)},
-            {MoveUp, New VerbTypeDescriptor("Move Up", Function(actor)
-                                                           Return CanMove(actor)
-                                                       End Function,
+            {MoveUp, New VerbTypeDescriptor(MoveUp, "Move Up", Function(actor)
+                                                                   Return CanMove(actor)
+                                                               End Function,
                 Sub(actor)
                     Move(actor, Facing.Up)
                 End Sub, visible:=False)},
-            {MoveRight, New VerbTypeDescriptor("Move Right", Function(actor)
-                                                                 Return CanMove(actor)
-                                                             End Function,
+            {MoveRight, New VerbTypeDescriptor(MoveRight, "Move Right", Function(actor)
+                                                                            Return CanMove(actor)
+                                                                        End Function,
                 Sub(actor)
                     Move(actor, Facing.Right)
 
                 End Sub, visible:=False)},
-            {MoveDown, New VerbTypeDescriptor("Move Down", Function(actor)
-                                                               Return CanMove(actor)
-                                                           End Function,
+            {MoveDown, New VerbTypeDescriptor(MoveDown, "Move Down", Function(actor)
+                                                                         Return CanMove(actor)
+                                                                     End Function,
                 Sub(actor)
                     Move(actor, Facing.Down)
 
                 End Sub, visible:=False)},
-            {MoveLeft, New VerbTypeDescriptor("Move Left", Function(actor)
-                                                               Return CanMove(actor)
-                                                           End Function,
+            {MoveLeft, New VerbTypeDescriptor(MoveLeft, "Move Left", Function(actor)
+                                                                         Return CanMove(actor)
+                                                                     End Function,
                 Sub(actor)
                     Move(actor, Facing.Left)
 
                 End Sub, visible:=False)},
-            {SPLORRPedia, New VerbTypeDescriptor("SPLORR!!Pedia", Function(actor)
-                                                                      Return True
-                                                                  End Function,
+            {SPLORRPedia, New VerbTypeDescriptor(SPLORRPedia, "SPLORR!!Pedia", Function(actor)
+                                                                                   Return True
+                                                                               End Function,
                 Sub(actor)
                     Return
                 End Sub)},
-            {Crew, New VerbTypeDescriptor("Crew...", Function(actor)
-                                                         Return actor.Family.HasChildren
-                                                     End Function,
+            {Crew, New VerbTypeDescriptor(Crew, "Crew...", Function(actor)
+                                                               Return actor.Family.HasChildren
+                                                           End Function,
                 Sub(actor)
                     Return
                 End Sub)},
-            {Vessel, New VerbTypeDescriptor("Vessel...", Function(actor)
-                                                             Return actor.Family.Parent IsNot Nothing
-                                                         End Function,
+            {Vessel, New VerbTypeDescriptor(Vessel, "Vessel...", Function(actor)
+                                                                     Return actor.Family.Parent IsNot Nothing
+                                                                 End Function,
                 Sub(actor)
                     Return
                 End Sub)}
