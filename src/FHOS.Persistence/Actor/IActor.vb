2eee212032f1ed6805f857e8f34a4b077d1d9ce7
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-05-20 09:37:09 -0500

    stuff

--- src/FHOS.Persistence/Actor/IActor.vb
+++ src/FHOS.Persistence/Actor/IActor.vb
@@ -1,9 +1,9 @@
 ﻿Public Interface IActor
     Inherits IEntity
+    ReadOnly Property ActorType As String
     ReadOnly Property KnownPlaces As IActorKnownPlaces
     ReadOnly Property Tutorial As IActorTutorial
     ReadOnly Property Family As IActorFamily
-    ReadOnly Property ActorType As String
     ReadOnly Property Properties As IActorProperties
     ReadOnly Property State As IActorState
     ReadOnly Property Equipment As IActorEquipment
