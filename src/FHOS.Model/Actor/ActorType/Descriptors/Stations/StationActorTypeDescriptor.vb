384d84cbd3eca9e00269ec35ac3e64fbf5106faf
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-05-30 05:49:46 -0500

    stations have factions again

--- src/FHOS.Model/Actor/ActorType/Descriptors/Stations/StationActorTypeDescriptor.vb
+++ src/FHOS.Model/Actor/ActorType/Descriptors/Stations/StationActorTypeDescriptor.vb
@@ -26,7 +26,7 @@ Friend MustInherit Class StationActorTypeDescriptor
     Protected Overrides Sub Initialize(actor As IActor)
         Dim planet = actor.State.Location.Map.GetCenterLocation().Actor
         actor.Properties.Name = MakeName(planet)
-        actor.Properties.Group = planet.Properties.Group.LegacyGroup
+        actor.Properties.Group = planet.Properties.Group.Parents.Single(Function(x) x.GroupType = GroupTypes.PlanetVicinity).Parents.Single(Function(x) x.GroupType = GroupTypes.Faction)
 
         actor.State.Location.LocationType = LocationTypes.ActorAdjacent
         For Each neighbor In actor.State.Location.GetEmptyNeighborsOfType(LocationTypes.Void)
