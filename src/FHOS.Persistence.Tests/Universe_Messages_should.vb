621335ec0a9351b584f774c97f828321dde97b34
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-05-31 05:43:59 -0500

    messages are characterized

---
+++ src/FHOS.Persistence.Tests/Universe_Messages_should.vb
@@ -0,0 +1,33 @@
+﻿Imports System.Net.Http
+
+Public Class Universe_Messages_should
+    <Fact>
+    Public Sub have_default_values_upon_instantiation()
+        Dim data As New UniverseData
+        Dim universe As New Universe(data)
+        Dim sut = universe.Messages
+        sut.HasAny.ShouldBeFalse
+        Should.Throw(Of InvalidOperationException)(Function() sut.Current)
+        Should.Throw(Of InvalidOperationException)(Sub() sut.Dismiss())
+        Should.NotThrow(Sub() sut.Add("test"))
+    End Sub
+    <Fact>
+    Public Sub add_and_dismiss_message()
+        Dim data As New UniverseData
+        Dim universe As New Universe(data)
+        Dim sut = universe.Messages
+
+        sut.HasAny.ShouldBeFalse
+
+        Const header = "test"
+
+        sut.Add(header)
+        sut.HasAny.ShouldBeTrue
+        sut.Current.ShouldNotBeNull
+        sut.Current.Header.ShouldBe(header)
+
+        sut.Dismiss()
+        sut.HasAny.ShouldBeFalse
+        Should.Throw(Of InvalidOperationException)(Function() sut.Current)
+    End Sub
+End Class
