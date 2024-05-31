535a3b27ff7332ee8cb4d0b6eb8e09076ecde88f
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-05-31 05:48:28 -0500

    avatar base tests

---
+++ src/FHOS.Persistence.Tests/Universe_Avatar_should.vb
@@ -0,0 +1,11 @@
+﻿Public Class Universe_Avatar_should
+    <Fact>
+    Sub have_default_values_upon_instantiation()
+        Dim data As New UniverseData
+        Dim universe As New Universe(data)
+        Dim sut = universe.Avatar
+        sut.Actor.ShouldBeNull
+        Should.Throw(Of InvalidOperationException)(Sub() sut.Pop())
+        Should.Throw(Of NullReferenceException)(Sub() sut.Push(Nothing))
+    End Sub
+End Class
