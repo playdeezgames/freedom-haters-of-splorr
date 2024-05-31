9dfeebbec2395bfa4b50b70d15985d7b19304c6a
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-05-31 16:27:53 -0500

    simple group tests

---
+++ src/FHOS.Persistence.Tests/Group_should.vb
@@ -0,0 +1,32 @@
+﻿Public Class Group_should
+    <Fact>
+    Sub have_an_id()
+        Dim sut As IGroup = CreateSut()
+        sut.Id.ShouldBe(0)
+    End Sub
+
+    Private Shared Function CreateSut() As IGroup
+        Const groupType = "group type"
+        Const groupName = "group name"
+        Return CreateGroup(groupType, groupName)
+    End Function
+
+    <Fact>
+    Sub have_flags()
+        Dim sut = CreateSut()
+        Const flagName = "flag name"
+        sut.Flags(flagName).ShouldBeFalse
+        sut.Flags(flagName) = True
+        sut.Flags(flagName).ShouldBeTrue
+        sut.Flags(flagName) = False
+        sut.Flags(flagName).ShouldBeFalse
+    End Sub
+    <Fact>
+    Sub have_universe_reference()
+        Dim sut = CreateSut()
+        sut.Universe.ShouldNotBeNull
+        'prove that the universe contains the sut
+        sut.Universe.Groups.Count.ShouldBe(1)
+        sut.Universe.Groups.First().Id.ShouldBe(sut.Id)
+    End Sub
+End Class
--- src/FHOS.Persistence.Tests/Utility.vb
+++ src/FHOS.Persistence.Tests/Utility.vb
@@ -1,14 +1,19 @@
 ﻿Friend Module Utility
     Friend Function CreateAvatar(Optional universe As IUniverse = Nothing, Optional data As UniverseData = Nothing) As IAvatar
-        Return If(universe, CreateUniverse(data)).Avatar
+        Return CreateUniverse(universe, data).Avatar
     End Function
     Friend Function CreateMessages(Optional universe As IUniverse = Nothing, Optional data As UniverseData = Nothing) As IMessages
-        Return If(universe, CreateUniverse(data)).Messages
+        Return CreateUniverse(universe, data).Messages
     End Function
     Friend Function CreateUniverseFactory(Optional universe As IUniverse = Nothing, Optional data As UniverseData = Nothing) As IUniverseFactory
-        Return If(universe, CreateUniverse(data)).Factory
+        Return CreateUniverse(universe, data).Factory
     End Function
-    Friend Function CreateUniverse(Optional data As UniverseData = Nothing) As IUniverse
-        Return New Universe(If(data, New UniverseData))
+    Friend Function CreateUniverse(Optional universe As IUniverse = Nothing, Optional data As UniverseData = Nothing) As IUniverse
+        Dim check = universe Is Nothing OrElse data Is Nothing
+        check.ShouldBeTrue("When bootstrapping a universe, either supply a universe already or the data behind it, or neither, but not both!")
+        Return If(universe, New Universe(If(data, New UniverseData)))
+    End Function
+    Friend Function CreateGroup(groupType As String, groupName As String, Optional universe As IUniverse = Nothing, Optional data As UniverseData = Nothing) As IGroup
+        Return CreateUniverseFactory(universe, data).CreateGroup(groupType, groupName)
     End Function
 End Module
