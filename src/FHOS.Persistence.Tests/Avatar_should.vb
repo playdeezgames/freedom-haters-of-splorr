7f7ceae436f3772c58c7000b1ce8f0dfe5115126
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-06-01 12:32:31 -0500

    avatar actor and pushy poppy

--- src/FHOS.Persistence.Tests/Avatar_should.vb
+++ src/FHOS.Persistence.Tests/Avatar_should.vb
@@ -6,4 +6,14 @@
         Should.Throw(Of InvalidOperationException)(Sub() sut.Pop())
         Should.Throw(Of NullReferenceException)(Sub() sut.Push(Nothing))
     End Sub
+    <Fact>
+    Sub push_pop_actor()
+        Dim universe = CreateUniverse()
+        Dim sut = CreateAvatar(universe)
+        Dim actor = CreateActor("map name", "map type", 3, 2, "location type", 1, 1, "actor type", "actor name", universe:=universe)
+        sut.Push(actor)
+        sut.Actor.Id.ShouldBe(actor.Id)
+        sut.Pop()
+        sut.Actor.ShouldBeNull
+    End Sub
 End Class
