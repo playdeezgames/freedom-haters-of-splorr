face2593dd0351c0c4aea07fa234fb7b7c385626
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-06-02 05:13:17 -0500

    we've got basic generator model tests

---
+++ src/FHOS.Model.Tests/UniverseGeneratorModel_should.vb
@@ -0,0 +1,36 @@
+﻿Public Class UniverseGeneratorModel_should
+    <Fact>
+    Sub start_generating_universe()
+        Dim sut = CreateSut()
+        sut.Start()
+        sut.StepsRemaining.ShouldBeGreaterThan(0)
+        sut.StepsCompleted.ShouldBe(0)
+        sut.Done.ShouldBeFalse
+    End Sub
+
+    <Fact>
+    Sub partially_generate_universe()
+        Dim sut = CreateSut()
+        sut.Start()
+        sut.Generate()
+        sut.StepsRemaining.ShouldBeGreaterThan(0)
+        sut.StepsCompleted.ShouldBeGreaterThan(0)
+        sut.Done.ShouldBeFalse
+    End Sub
+
+    <Fact>
+    Sub completely_generate_universe()
+        Dim sut = CreateSut()
+        sut.Start()
+        While Not sut.Done
+            sut.Generate()
+        End While
+        sut.StepsRemaining.ShouldBe(0)
+        sut.StepsCompleted.ShouldBeGreaterThan(0)
+        sut.Done.ShouldBeTrue
+    End Sub
+
+    Private Function CreateSut() As IUniverseGeneratorModel
+        Return New UniverseModel().Generator
+    End Function
+End Class
