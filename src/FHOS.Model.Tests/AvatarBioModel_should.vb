d860180cf7ece76bb109820bdd07ab0e7093aeb4
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-06-03 05:12:44 -0500

    AvatarBioModel_should

---
+++ src/FHOS.Model.Tests/AvatarBioModel_should.vb
@@ -0,0 +1,12 @@
+﻿Public Class AvatarBioModel_should
+    <Fact>
+    Sub have_default_values_upon_initialization()
+        Dim sut = CreateSut()
+        sut.HomePlanet.ShouldBeNull
+        sut.Group.ShouldNotBeNull
+    End Sub
+
+    Private Function CreateSut() As IAvatarBioModel
+        Return CreateOneStepUniverse(AddressOf BuildLonelyUniverse).State.Avatar.Bio
+    End Function
+End Class
