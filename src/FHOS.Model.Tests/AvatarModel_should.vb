dd7cb63dc04d315b0bf8834d310b7d1b44480a96
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-06-03 05:10:10 -0500

    AvatarModel_should

---
+++ src/FHOS.Model.Tests/AvatarModel_should.vb
@@ -0,0 +1,20 @@
+﻿Public Class AvatarModel_should
+    <Fact>
+    Sub have_default_values_upon_initialization()
+        Dim sut = CreateSut()
+        sut.Bio.ShouldNotBeNull
+        sut.Interaction.ShouldNotBeNull
+        sut.Stack.ShouldNotBeNull
+        sut.StarGate.ShouldNotBeNull
+        sut.State.ShouldNotBeNull
+        sut.Status.ShouldNotBeNull
+        sut.Tutorial.ShouldNotBeNull
+        sut.Verbs.ShouldNotBeNull
+        sut.Vessel.ShouldNotBeNull
+        sut.Wallet.ShouldNotBeNull
+    End Sub
+
+    Private Function CreateSut() As IAvatarModel
+        Return CreateOneStepUniverse(AddressOf BuildLonelyUniverse).State.Avatar
+    End Function
+End Class
