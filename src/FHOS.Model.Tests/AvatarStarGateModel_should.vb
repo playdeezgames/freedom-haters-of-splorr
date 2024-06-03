108007ddd59e672dd59e0641ff009cfd830300de
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-06-03 05:42:30 -0500

    avatar stargate

---
+++ src/FHOS.Model.Tests/AvatarStarGateModel_should.vb
@@ -0,0 +1,14 @@
+﻿Public Class AvatarStarGateModel_should
+    <Fact>
+    Sub have_default_values_upon_initialization()
+        Dim sut = CreateSut()
+        sut.IsActive.ShouldBeFalse
+        Should.Throw(Of KeyNotFoundException)(Function() sut.AvailableGates.ToList())
+        Should.Throw(Of NullReferenceException)(Sub() sut.Enter(Nothing))
+        Should.NotThrow(Sub() sut.Leave())
+    End Sub
+
+    Private Shared Function CreateSut() As IAvatarStarGateModel
+        Return CreateOneStepUniverse(AddressOf BuildLonelyUniverse).State.Avatar.StarGate
+    End Function
+End Class
