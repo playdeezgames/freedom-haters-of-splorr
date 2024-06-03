32540b932d7f84fd35a4a2bd76bd9a8d8d8663cd
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-06-03 04:08:29 -0500

    default values for state model

---
+++ src/FHOS.Model.Tests/UniverseStateModel_should.vb
@@ -0,0 +1,19 @@
+﻿Public Class UniverseStateModel_should
+    <Fact>
+    Sub have_default_values_upon_initialization()
+        Dim sut = CreateSut()
+        sut.Turn.ShouldBe(1)
+        sut.Board.ShouldNotBeNull
+        sut.Avatar.ShouldBeNull
+        sut.Messages.ShouldNotBeNull
+    End Sub
+
+    Private Function CreateSut() As IUniverseStateModel
+        Dim model = New UniverseModel(initializer:=New EmptyUniverseInitializer(0), generationTimeSlice:=0.0)
+        model.Generator.Start()
+        While Not model.Generator.Done
+            model.Generator.Generate()
+        End While
+        Return model.State
+    End Function
+End Class
