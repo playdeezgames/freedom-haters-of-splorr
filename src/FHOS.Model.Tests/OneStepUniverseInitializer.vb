b562f42e23c0ae90c70795b2126040d1cc0e1a8e
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-06-03 04:16:04 -0500

    minmal universe ftw

---
+++ src/FHOS.Model.Tests/OneStepUniverseInitializer.vb
@@ -0,0 +1,28 @@
+﻿Public Class OneStepUniverseInitializer
+    Implements IInitializer
+
+    Private ReadOnly doStuff As Action(Of IUniverse, EmbarkSettings)
+    Sub New(doStuff As Action(Of IUniverse, EmbarkSettings))
+        Me.doStuff = doStuff
+    End Sub
+
+    Public ReadOnly Property StepsRemaining As Integer Implements IInitializer.StepsRemaining
+        Get
+            Return 0
+        End Get
+    End Property
+
+    Public ReadOnly Property StepsDone As Integer Implements IInitializer.StepsDone
+        Get
+            Return 0
+        End Get
+    End Property
+
+    Public Sub Start(universe As IUniverse, settings As EmbarkSettings) Implements IInitializer.Start
+        doStuff(universe, settings)
+    End Sub
+
+    Public Function Execute() As Boolean Implements IInitializer.Execute
+        Return False
+    End Function
+End Class
--- src/FHOS.Model.Tests/UniverseStateModel_should.vb
+++ src/FHOS.Model.Tests/UniverseStateModel_should.vb
@@ -4,16 +4,21 @@
         Dim sut = CreateSut()
         sut.Turn.ShouldBe(1)
         sut.Board.ShouldNotBeNull
-        sut.Avatar.ShouldBeNull
+        sut.Avatar.ShouldNotBeNull
         sut.Messages.ShouldNotBeNull
     End Sub
 
     Private Function CreateSut() As IUniverseStateModel
-        Dim model = New UniverseModel(initializer:=New EmptyUniverseInitializer(0), generationTimeSlice:=0.0)
+        Dim model = New UniverseModel(initializer:=New OneStepUniverseInitializer(AddressOf BuildUniverse), generationTimeSlice:=0.0)
         model.Generator.Start()
         While Not model.Generator.Done
             model.Generator.Generate()
         End While
         Return model.State
     End Function
+
+    Private Sub BuildUniverse(universe As IUniverse, settings As EmbarkSettings)
+        Dim map = universe.Factory.CreateMap("map name", "map type", 1, 1, "location type")
+        universe.Avatar.Push(map.GetLocation(0, 0).CreateActor("actor type", "actor name"))
+    End Sub
 End Class
