e8d765b5689eb0c9ac7dbf122c7846476a4d341f
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-06-03 05:48:56 -0500

    AvatarStatusModel_should

---
+++ src/FHOS.Model.Tests/AvatarStatusModel_should.vb
@@ -0,0 +1,13 @@
+﻿Public Class AvatarStatusModel_should
+    <Fact>
+    Sub have_default_values_upon_initialization()
+        Dim sut = CreateSut()
+        Should.Throw(Of NullReferenceException)(Function() sut.GameOver)
+        Should.Throw(Of NullReferenceException)(Function() sut.Bankrupt)
+        Should.Throw(Of NullReferenceException)(Function() sut.Dead)
+    End Sub
+
+    Private Function CreateSut() As IAvatarStatusModel
+        Return CreateOneStepUniverse(AddressOf BuildLonelyUniverse).State.Avatar.Status
+    End Function
+End Class
