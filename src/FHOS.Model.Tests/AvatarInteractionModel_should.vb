a936cb2df38296188b51857196d50e77e579daf2
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-06-03 05:25:39 -0500

    AvatarInteractionModel_should

---
+++ src/FHOS.Model.Tests/AvatarInteractionModel_should.vb
@@ -0,0 +1,14 @@
+﻿Public Class AvatarInteractionModel_should
+    <Fact>
+    Sub have_default_values_upon_initialization()
+        Dim sut = CreateSut()
+        Should.NotThrow(Sub() sut.Leave())
+        sut.IsActive.ShouldBeFalse
+        Should.Throw(Of NullReferenceException)(Function() sut.AvailableChoices)
+        Should.Throw(Of NullReferenceException)(Function() sut.Lines)
+    End Sub
+
+    Private Function CreateSut() As IAvatarInteractionModel
+        Return CreateOneStepUniverse(AddressOf BuildLonelyUniverse).State.Avatar.Interaction
+    End Function
+End Class
