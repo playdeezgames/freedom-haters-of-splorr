46fd4ee3538968e27339f5b88d09cc188bec38b2
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-06-03 04:20:10 -0500

    BoardModel_should

---
+++ src/FHOS.Model.Tests/BoardModel_should.vb
@@ -0,0 +1,21 @@
+﻿Public Class BoardModel_should
+    <Fact>
+    Sub have_default_values_upon_initialization()
+        Dim sut = CreateSut()
+        sut.GetLocation((0, 0)).ShouldNotBeNull
+    End Sub
+
+    Private Function CreateSut() As IBoardModel
+        Dim model = New UniverseModel(initializer:=New OneStepUniverseInitializer(AddressOf BuildUniverse), generationTimeSlice:=0.0)
+        model.Generator.Start()
+        While Not model.Generator.Done
+            model.Generator.Generate()
+        End While
+        Return model.State.Board
+    End Function
+
+    Private Sub BuildUniverse(universe As IUniverse, settings As EmbarkSettings)
+        Dim map = universe.Factory.CreateMap("map name", "map type", 1, 1, "location type")
+        universe.Avatar.Push(map.GetLocation(0, 0).CreateActor("actor type", "actor name"))
+    End Sub
+End Class
