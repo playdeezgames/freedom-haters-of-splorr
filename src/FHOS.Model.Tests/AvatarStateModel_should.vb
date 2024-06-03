9bc974322529d7bf008445c82bddac6924b25268
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-06-03 05:45:30 -0500

    AvatarStateModel_should

---
+++ src/FHOS.Model.Tests/AvatarStateModel_should.vb
@@ -0,0 +1,14 @@
+﻿Public Class AvatarStateModel_should
+    <Fact>
+    Sub have_default_values_upon_initialization()
+        Dim sut = CreateSut()
+        sut.Position.X.ShouldBe(0)
+        sut.Position.Y.ShouldBe(0)
+        sut.MapName.ShouldBe("map name")
+        sut.Scrap.ShouldBe(0)
+    End Sub
+
+    Private Function CreateSut() As IAvatarStateModel
+        Return CreateOneStepUniverse(AddressOf BuildLonelyUniverse).State.Avatar.State
+    End Function
+End Class
