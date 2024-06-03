e6c266f83f7e6c3308e5f0f540a0b395ec06266a
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-06-03 04:03:56 -0500

    UniverseSettingsModel_should

---
+++ src/FHOS.Model.Tests/UniverseSettingsModel_should.vb
@@ -0,0 +1,13 @@
+﻿Public Class UniverseSettingsModel_should
+    Private Function CreateSut() As IUniverseSettingsModel
+        Return New UniverseModel(initializer:=New EmptyUniverseInitializer(2), generationTimeSlice:=0.0).Settings
+    End Function
+    <Fact>
+    Sub have_default_values_upon_initialization()
+        Dim sut = CreateSut()
+        sut.GalacticAge.ShouldNotBeNull
+        sut.GalacticDensity.ShouldNotBeNull
+        sut.StartingWealth.ShouldNotBeNull
+        sut.FactionCount.ShouldNotBeNull
+    End Sub
+End Class
