be8b7e9cf45e1395c0931a93ca0971616d71c419
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-06-04 04:11:13 -0500

    AvatarVerbsModel_should

---
+++ src/FHOS.Model.Tests/AvatarVerbsModel_should.vb
@@ -0,0 +1,13 @@
+﻿Public Class AvatarVerbsModel_should
+    <Fact>
+    Sub have_default_values_upon_initialization()
+        Dim sut = CreateSut()
+        sut.Available.Count.ShouldBe(2)
+        sut.HasAny.ShouldBeTrue
+        Should.Throw(Of ArgumentNullException)(Sub() sut.Perform(Nothing))
+    End Sub
+
+    Private Function CreateSut() As IAvatarVerbsModel
+        Return CreateOneStepUniverse(AddressOf BuildLonelyUniverse).State.Avatar.Verbs
+    End Function
+End Class
