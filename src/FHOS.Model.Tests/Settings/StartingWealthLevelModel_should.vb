a0f6c22ae2bdbbc6bf6022eeebde471bee988b4c
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-06-04 04:41:40 -0500

    StartingWealthLevelModel_should

---
+++ src/FHOS.Model.Tests/Settings/StartingWealthLevelModel_should.vb
@@ -0,0 +1,31 @@
+﻿Public Class StartingWealthLevelModel_should
+    <Fact>
+    Sub have_default_values_upon_initialization()
+        Dim sut = CreateSut()
+        sut.Current.ShouldNotBeNull
+        sut.CurrentName.ShouldNotBeNull
+        sut.Options.Count.ShouldBe(5)
+    End Sub
+    <Fact>
+    Sub set_wealth_level()
+        Dim sut = CreateSut()
+        Dim item = sut.Options.First.Item
+        sut.SetWealthLevel(item)
+        sut.Current.ShouldBe(item)
+    End Sub
+
+    Private Function CreateSut() As IStartingWealthLevelModel
+        Return CreateOneStepUniverse(
+            AddressOf BuildLonelyUniverse,
+            writeStringToFile:=AddressOf WriteFile,
+            readStringFromFile:=AddressOf ReadFile).Settings.StartingWealth
+    End Function
+
+    Private Function ReadFile(arg As String) As String
+        Throw New NotImplementedException()
+    End Function
+
+    Private Sub WriteFile(arg1 As String, arg2 As String)
+        'do nothing
+    End Sub
+End Class
