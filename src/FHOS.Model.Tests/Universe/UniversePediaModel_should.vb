5061e3925bee4c1c1e949d90897a858eeb56350d
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-06-04 04:47:00 -0500

    UniversePediaModel_should

---
+++ src/FHOS.Model.Tests/Universe/UniversePediaModel_should.vb
@@ -0,0 +1,14 @@
+﻿Public Class UniversePediaModel_should
+    <Fact>
+    Sub have_initial_values_upon_initialization()
+        Dim sut = CreateSut()
+        sut.FactionList.ShouldBeEmpty
+        sut.StarSystemList.ShouldBeEmpty
+        sut.PlanetVicinityList.ShouldBeEmpty
+        sut.SatelliteList.ShouldBeEmpty
+    End Sub
+
+    Private Function CreateSut() As IUniversePediaModel
+        Return CreateOneStepUniverse(AddressOf BuildLonelyUniverse).Pedia
+    End Function
+End Class
