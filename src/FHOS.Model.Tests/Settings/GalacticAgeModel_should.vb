bab721c79a51f0f8e4119da69b56095cebb75a6d
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-06-04 04:31:03 -0500

    GalacticAgeModel_should

---
+++ src/FHOS.Model.Tests/Settings/GalacticAgeModel_should.vb
@@ -0,0 +1,20 @@
+﻿Public Class GalacticAgeModel_should
+    <Fact>
+    Sub have_default_values_upon_initialization()
+        Dim sut = CreateSut()
+        sut.Current.ShouldNotBeNull
+        sut.CurrentName.ShouldNotBeNull
+        sut.Options.Count.ShouldBe(3)
+    End Sub
+    <Fact>
+    Sub set_age()
+        Dim sut = CreateSut()
+        Dim item = sut.Options.First.Item
+        sut.SetAge(item)
+        sut.Current.ShouldBe(item)
+    End Sub
+
+    Private Function CreateSut() As IGalacticAgeModel
+        Return CreateOneStepUniverse(AddressOf BuildLonelyUniverse).Settings.GalacticAge
+    End Function
+End Class
