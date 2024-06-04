1479f32910ef3cef39c91969dfd20837aabd6e0c
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-06-04 04:17:20 -0500

    AvatarWalletModel_should

---
+++ src/FHOS.Model.Tests/AvatarWalletModel_should.vb
@@ -0,0 +1,12 @@
+﻿Public Class AvatarWalletModel_should
+    <Fact>
+    Sub have_default_values_upon_initialization()
+        Dim sut = CreateSut()
+        Should.Throw(Of NullReferenceException)(Function() sut.Jools)
+        Should.Throw(Of NullReferenceException)(Function() sut.MinimumJools)
+    End Sub
+
+    Private Function CreateSut() As IAvatarWalletModel
+        Return CreateOneStepUniverse(AddressOf BuildLonelyUniverse).State.Avatar.Wallet
+    End Function
+End Class
