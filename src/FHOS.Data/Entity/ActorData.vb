b4f99fa59c423bd8074747903464a89f980fc432
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-06-14 08:25:07 -0500

    minor changes to data

--- src/FHOS.Data/Entity/ActorData.vb
+++ src/FHOS.Data/Entity/ActorData.vb
@@ -1,6 +1,5 @@
 ﻿Public Class ActorData
     Inherits EntityData
-    Public Property Tutorials As New Queue(Of String)
     Public Property Children As New HashSet(Of Integer)
     Public Property Equipment As New HashSet(Of Integer)
     Public Property YokedActors As New Dictionary(Of String, Integer)
--- src/FHOS.Data/Entity/EntityData.vb
+++ src/FHOS.Data/Entity/EntityData.vb
@@ -1,6 +1,4 @@
-﻿Imports System.Text.Json.Serialization
-
-Public MustInherit Class EntityData
+﻿Public MustInherit Class EntityData
     Property Flags As New HashSet(Of String)
     Property Statistics As New Dictionary(Of String, Integer)
     Property Metadatas As New Dictionary(Of String, String)
