7e68e8f4f2b3e13b2da63521205b3778b61ae788
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-06-14 04:24:26 -0500

    brought back the group test

---
+++ src/FHOS.Model.Tests/GroupModel_should.vb
@@ -0,0 +1,26 @@
+﻿Public Class GroupModel_should
+    <Fact>
+    Sub have_default_values_upon_initialization()
+        Dim sut = CreateSut()
+        sut.Name.ShouldBe("group name")
+        Should.Throw(Of InvalidOperationException)(Function() sut.Authority)
+        Should.Throw(Of InvalidOperationException)(Function() sut.Standards)
+        Should.Throw(Of InvalidOperationException)(Function() sut.Conviction)
+        Should.Throw(Of InvalidOperationException)(Function() sut.PlanetCount)
+        Should.Throw(Of InvalidOperationException)(Function() sut.SatelliteCount)
+        Should.Throw(Of InvalidOperationException)(Function() sut.Position)
+        sut.PlanetList.ShouldBeEmpty
+        sut.SatelliteList.ShouldBeEmpty
+        sut.FactionsPresent.ShouldBeEmpty
+        sut.Pedia.ShouldNotBeNull
+        sut.StarSystem.ShouldBeNull
+        sut.Planet.ShouldBeNull
+        sut.Faction.ShouldBeNull
+        sut.StarTypeName.ShouldBeNull
+        Should.Throw(Of InvalidOperationException)(Function() sut.RelationNameTo(sut))
+    End Sub
+
+    Private Function CreateSut() As IGroupModel
+        Return CreateOneStepUniverse(AddressOf BuildLonelyUniverse).State.Avatar.Bio.Group
+    End Function
+End Class
