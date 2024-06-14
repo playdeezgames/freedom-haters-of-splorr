0116d3b30f4087ba16449a1551a4871fd73ad47a
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-06-14 14:55:55 -0500

    ready for facing switch

---
+++ src/FHOS.Model/Types/Facing/FacingDescriptor.vb
@@ -0,0 +1,9 @@
+﻿Friend Class FacingDescriptor
+    Friend ReadOnly Facing As Integer
+    Friend ReadOnly Delta As (X As Integer, Y As Integer)
+
+    Public Sub New(up As Integer, value As (X As Integer, Y As Integer))
+        Me.facing = up
+        Me.delta = value
+    End Sub
+End Class
---
+++ src/FHOS.Model/Types/Facing/FacingTypes.vb
@@ -0,0 +1,22 @@
+﻿Friend Module FacingTypes
+    Friend Const Up = 0
+    Friend Const Right = 1
+    Friend Const Down = 2
+    Friend Const Left = 3
+    Friend ReadOnly Descriptors As Dictionary(Of Integer, FacingDescriptor) =
+        New List(Of FacingDescriptor) From
+        {
+            New FacingDescriptor(Up, (0, -1)),
+            New FacingDescriptor(Right, (1, 0)),
+            New FacingDescriptor(Down, (0, 1)),
+            New FacingDescriptor(Left, (-1, 0))
+        }.ToDictionary(Function(x) x.Facing, Function(x) x)
+    Friend ReadOnly All As IEnumerable(Of Integer) = Descriptors.Keys
+    Friend ReadOnly Deltas As (X As Integer, Y As Integer)() =
+        {
+            (0, -1),
+            (1, 0),
+            (0, 1),
+            (-1, 0)
+        }
+End Module
