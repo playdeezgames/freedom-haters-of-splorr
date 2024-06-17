23e47a36fec28b46dc3cd0e7d234abbf98940b27
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-06-17 05:21:54 -0500

    update nugets

--- src/FHOS.Data/Entity/Base/IEntityData.vb
+++ src/FHOS.Data/Entity/Base/IEntityData.vb
@@ -1,5 +1,13 @@
 ﻿Public Interface IEntityData
     Property Flags As HashSet(Of String)
+    'Sub SetFlag(flagName As String)
+    'Sub ClearFlag(flagName As String)
+    'Function HasFlag(flagName As String) As Boolean
+    'Sub ClearFlags()
     Property Statistics As Dictionary(Of String, Integer)
+    'Sub SetStatistic(statisticType As String, value As Integer?)
+    'Function GetStatistic(statisticType As String) As Integer?
     Property Metadatas As Dictionary(Of String, String)
+    'Sub SetMetadata(metadataType As String, value As String)
+    'Function GetMetadata(metadataType As String) As String
 End Interface
