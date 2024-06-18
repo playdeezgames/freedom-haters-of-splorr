ac943ae0a62999294e26a54abaf564451435ca8e
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-06-18 05:53:49 -0500

    commented out stubs

--- src/FHOS.Data/Entity/Map/IMapData.vb
+++ src/FHOS.Data/Entity/Map/IMapData.vb
@@ -1,5 +1,9 @@
 ﻿Public Interface IMapData
     Inherits IEntityData
     Property Locations As List(Of Integer)
+    'Sub SetLocation(index As Integer, locationId As Integer)
+    'Function GetLocation(index As Integer) As Integer
     Property YokedGroups As Dictionary(Of String, Integer)
+    'Sub SetYokedGroup(yokeType As String, groupId As Integer?)
+    'Function GetYokedGroup(yokeType As String) As Integer?
 End Interface
