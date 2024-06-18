d12431ff0a63c4570fe234a7f2e3d9e94e6a63f1
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-06-18 05:33:23 -0500

    group data parents and children commented out stubs

--- src/FHOS.Data/Entity/Group/IGroupData.vb
+++ src/FHOS.Data/Entity/Group/IGroupData.vb
@@ -1,5 +1,15 @@
 ﻿Public Interface IGroupData
     Inherits IEntityData
     Property Children As HashSet(Of Integer)
+    'ReadOnly Property HasChildren As Boolean
+    'ReadOnly Property AllChildren As IEnumerable(Of Integer)
+    'Sub AddChild(childId As Integer)
+    'Sub RemoveChild(childId As Integer)
+    'Function HasChild(childId As Integer) As Boolean
     Property Parents As HashSet(Of Integer)
+    'ReadOnly Property HasParents As Boolean
+    'ReadOnly Property AllParents As IEnumerable(Of Integer)
+    'Sub AddParent(parentId As Integer)
+    'Sub RemoveParent(parentId As Integer)
+    'Function HasParent(parentId As Integer) As Boolean
 End Interface
