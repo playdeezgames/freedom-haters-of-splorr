4ae122f09c92cbd3c74221a057cbdf8635e4369c
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-06-30 18:29:14 -0500

    some whitespacing

--- src/FHOS.Data/Entity/Base/IEntityData.vb
+++ src/FHOS.Data/Entity/Base/IEntityData.vb
@@ -2,8 +2,10 @@
     Function HasFlag(flagName As String) As Boolean
     Sub SetFlag(flagName As String)
     Sub ClearFlag(flagName As String)
+
     Sub SetStatistic(statisticType As String, value As Integer?)
     Function GetStatistic(statisticType As String) As Integer?
+
     Sub SetMetadata(metadataType As String, value As String)
     Function GetMetadata(metadataType As String) As String
 End Interface
--- src/FHOS.Presentation/Tutorial/TutorialDetailLine.vb
+++
@@ -1,8 +0,0 @@
-﻿Public Class TutorialDetailLine
-    ReadOnly Property Text As String
-    ReadOnly Property Hue As Integer
-    Sub New(text As String, hue As Integer)
-        Me.Text = text
-        Me.Hue = hue
-    End Sub
-End Class
