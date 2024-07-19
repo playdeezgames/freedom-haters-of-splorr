3411447e7e92b39435c55bcbbe4bf41e7d3aad4f
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-07-19 05:09:21 -0500

    SIGMO!

--- src/FHOS/Program.vb
+++ src/FHOS/Program.vb
@@ -1,5 +1,7 @@
 Imports FHOS.Model
 Imports FHOS.Presentation
+Imports Spectre.Console
+Imports SPLORR.Presentation
 Imports SPLORR.Presentation.Spectre
 
 Module Program
@@ -10,6 +12,11 @@ Module Program
         Catch ex As PlatformNotSupportedException
             'nom!
         End Try
-        MainMenuState.Start(New UniverseModel, New UIContext)
+        Dim image = New CanvasImage("SIGMO.png")
+        AnsiConsole.Write(image)
+        Dim ui = New UIContext
+        ui.WriteFiglet((Mood.Title, "SIGMO"))
+        ui.Message((Mood.Prompt, String.Empty))
+        MainMenuState.Start(New UniverseModel, ui)
     End Sub
 End Module
