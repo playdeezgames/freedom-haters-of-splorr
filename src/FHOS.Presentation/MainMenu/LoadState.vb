e99252ce78af34c5c5aad748fae9e0dd9f7286ea
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-07-19 04:21:29 -0500

    Reapply "load state"
    This reverts commit b69178b4366c916e39521f29d47bc9f634b65e98.

--- src/FHOS.Presentation/MainMenu/LoadState.vb
+++ src/FHOS.Presentation/MainMenu/LoadState.vb
@@ -10,6 +10,24 @@ Friend Class LoadState
     End Sub
 
     Public Overrides Function Run() As IState
-        Throw New NotImplementedException()
+        ui.Clear()
+        Dim loadableSlots = model.LoadableSlots()
+        If Not loadableSlots.Any Then
+            ui.Message((Mood.Danger, "No Saves Exist!"))
+            Return endState
+        End If
+        Dim menu As New List(Of (String, Integer?)) From
+            {
+                (Choices.Leave, Nothing)
+            }
+        menu.AddRange(loadableSlots.Select(Of (String, Integer?))(Function(x) (model.GetSlotName(x), x)))
+        Dim choice = ui.Choose(Of Integer?)(
+            (Mood.Prompt, Prompts.LoadGame),
+            menu.ToArray)
+        If Not choice.HasValue Then
+            Return endState
+        End If
+        model.LoadGame(choice.Value)
+        Return New NeutralState(model, ui, endState)
     End Function
 End Class
