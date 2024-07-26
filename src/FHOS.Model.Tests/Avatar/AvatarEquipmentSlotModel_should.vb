ae58e7dec22a2bd70c86b55c32538ea5d26cd2a7
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-07-26 04:22:35 -0500

    AvatarEquipmentSlotModel_should

---
+++ src/FHOS.Model.Tests/Avatar/AvatarEquipmentSlotModel_should.vb
@@ -0,0 +1,12 @@
+﻿Public Class AvatarEquipmentSlotModel_should
+    <Fact>
+    Sub have_default_values_upon_initialization()
+        Dim sut = CreateSut()
+        sut.Item.DisplayName.ShouldBe("StarLume Fuel Mark I")
+        sut.SlotName.ShouldBe("Fuel Supply")
+    End Sub
+
+    Private Function CreateSut() As IAvatarEquipmentSlotModel
+        Return CreateOneStepUniverse(AddressOf BuildShipyardUniverse).State.Avatar.Equipment.Slots.First
+    End Function
+End Class
