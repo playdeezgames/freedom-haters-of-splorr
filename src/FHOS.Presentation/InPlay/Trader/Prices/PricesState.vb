49fdb50539bda68472cf137b29f05f242de9b48b
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-08-13 12:23:59 -0500

    delete comment

--- src/FHOS.Presentation/InPlay/Trader/Prices/PricesState.vb
+++ src/FHOS.Presentation/InPlay/Trader/Prices/PricesState.vb
@@ -35,25 +35,4 @@ Friend Class PricesState
     Protected Overrides Function OnCancel() As IState
         Return New TraderState(model, ui, endState)
     End Function
-    'Inherits BaseState
-    'Implements IState
-
-    'Public Sub New(model As IUniverseModel, ui As IUIContext, endState As IState)
-    '    MyBase.New(model, ui, endState)
-    'End Sub
-
-    'Public Overrides Function Run() As IState
-    '    ui.Clear()
-    '    ui.WriteLine((Mood.Info, $"Jools: {model.State.Avatar.Jools}"))
-    '    Dim menuItems As New List(Of (String, IAvatarTraderPriceModel)) From
-    '        {
-    '            (Choices.Cancel, Nothing)
-    '        }
-    '    menuItems.AddRange(model.State.Avatar.Trader.Prices.Select(Function(x) ($"{x.Name}@{x.UnitPrice} (You have {x.InventoryQuantity})", x)))
-    '    Dim choice = ui.Choose((Mood.Prompt, String.Empty), menuItems.ToArray)
-    '    If choice Is Nothing Then
-    '        Return New TraderState(model, ui, endState)
-    '    End If
-    '    Return New BuyQuantityState(model, ui, endState, choice)
-    'End Function
 End Class
