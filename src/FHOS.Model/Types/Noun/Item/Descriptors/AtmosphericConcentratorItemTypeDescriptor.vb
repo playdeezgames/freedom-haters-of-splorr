a174a990de63bbf56cf8db0e71a23469d8794459
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-08-15 04:59:09 -0500

    put price back for air scoop

--- src/FHOS.Model/Types/Noun/Item/Descriptors/AtmosphericConcentratorItemTypeDescriptor.vb
+++ src/FHOS.Model/Types/Noun/Item/Descriptors/AtmosphericConcentratorItemTypeDescriptor.vb
@@ -10,7 +10,7 @@ Friend Class AtmosphericConcentratorItemTypeDescriptor
             "AeroSynth Recharger",
             New Dictionary(Of String, Double),
             3,
-            price:=500,
+            price:=5000,
             installFee:=25,
             uninstallFee:=15)
     End Sub
